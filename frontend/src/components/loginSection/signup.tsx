8a56e82598d0ebfb442c7bf42d1be5ead6add968
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-04-13 18:17:32 -0500

    integrated form for sign up section plus css

--- frontend/src/components/loginSection/signup.tsx
+++ frontend/src/components/loginSection/signup.tsx
@@ -1,14 +1,36 @@
-
-
+import './signup.css'
+import { useState } from 'react';
 
 
 const SignUpComponent = () => {
 
+    //use state 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     return (
        <section>
-        <div>
+        <div className='welcome-div'>
             <h1>Welcome !</h1>
         </div>
+
+        <div className='form-section'>
+        <div className='form-card'>
+            <form action="">
+                <h3>Sign Up</h3>
+                <input type="text" value={username} placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
+                <input type="email" value={email} placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" value={password} placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+                <input type="password" value={confirmPassword} placeholder='confirm password' onChange={(e) => setConfirmPassword(e.target.value)}/>
+                <button>SIGN UP</button>
+            </form>
+            <div className='already-have-account-div'>
+                <p>Already have an account? <span className='span-sign-in'>Sign in</span></p>
+            </div>
+        </div>
+        </div>
+
        </section>
     )
 }

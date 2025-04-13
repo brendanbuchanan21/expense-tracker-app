98daf89fb904e06f89974de68e25d443c95d5ac6
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-04-13 17:21:15 -0500

    intro page for expense tracker set up

---
+++ frontend/src/components/loginSection/intro.tsx
@@ -0,0 +1,48 @@
+import './intro.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const IntroComponent = () => {
+
+
+    let cards = ['Track Expenses', 'See Your Spending Habits', 'Take Control Of Your Finances'];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const navigate = useNavigate();
+
+    const handleNextPage = () => {
+        navigate('/welcome')
+    }
+
+
+    return (
+        <section>
+            <div className="intro-div">
+                <h1>Unlock Your Financial Clarity</h1>
+            </div>
+            <div className="card-container">
+             <div className="card">
+               <p>{cards[currentIndex]}</p>
+             </div>
+            </div>
+
+            <div className='dot-container'>
+                {cards.map((_, index) => (
+                    <span 
+                    key={index}
+                    className={`dot ${currentIndex === index ? 'active' : ''}`}
+                    onClick={() => setCurrentIndex(index)}
+                    />
+                ))}
+            </div>
+
+            <div className='get-started-div'>
+                <button onClick={handleNextPage}>Create New Account</button>
+            </div>
+            
+        </section>
+    )
+}
+
+export default IntroComponent;
---
+++ frontend/src/components/loginSection/signup.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+const SignUpComponent = () => {
+
+    return (
+       <section>
+        <div>
+            <h1>Welcome !</h1>
+        </div>
+       </section>
+    )
+}
+
+export default SignUpComponent;

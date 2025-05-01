b43bce79297fcb737f28c63e85e67fc9c51f8094
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-05-01 10:01:59 -0500

    add transaction reducer in redux

--- frontend/src/redux/accountSlice.ts
+++ frontend/src/redux/accountSlice.ts
@@ -1,6 +1,17 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 
+// each account needs to be able to have transactions and adjust the 
+// balance for each account
+
+export interface Transaction {
+    description: string;
+    date: string;
+    type: string;
+    amount: number;
+    category: string;
+}
+
 export interface Account {
     userId: string;
     id?: number;
@@ -8,6 +19,7 @@ export interface Account {
     bankName: string;
     balance: number;
     typeOfAccount: string;
+    transactions?: Transaction[];
 }
 
 interface AccountState {
@@ -24,6 +36,17 @@ const accountSlice = createSlice({
     reducers: {
         addAccount: (state, action: PayloadAction<Account>) => {
             state.accounts.push(action.payload);
+        },
+        addTransaction: (state, action: PayloadAction<{ accountId: number; transaction: Transaction}>) => {
+            const { accountId, transaction } = action.payload;
+
+            const account = state.accounts.find((account) => account.id === accountId);
+            if (account) {
+                account.transactions?.push(transaction);
+                account.balance += transaction.type === "Depost" ? transaction.amount: -transaction.amount
+            } 
+           
+
         }
     }
 })
